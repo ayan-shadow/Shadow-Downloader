2a74d8435c60b7b3fd79d4792250460727114ce1
ayan-shadow <arkadiphaldar91@gmail.com>
2026-04-18 18:11:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def smart_fetch():
             'quiet': True,
             'no_warnings': True,
             'format': 'best',
+            'cookiefile': 'cookies.txt',  # <--- Cookies file yahan add ho gayi hai
             'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         }
 
